9dfa9f5e6a44c3421de7d6c28c02e584e088695a
Aashna-Verma <aashna.verma@outlook.com>
2022-12-27 01:50:43 -0500

    nav bar compleate

--- src/App.js
+++ src/App.js
@@ -1,56 +1,43 @@
 import './App.css';
+import Nav from "./nav"
+import axios from 'axios';
 
 // assets //
 import logo from './assets/Bread-sparkle-bigcircle.svg';
-import search from './assets/search.svg';
-import saved from './assets/saved.svg';
-import list from './assets/list.svg';
-import account from './assets/account.svg';
-import settings from './assets/settings.svg';
 
 
-function App() {
+
+const App = () => {
+  
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${API_KEY}&health=vegan&health=vegetarian`;
+
+  const getData = async () => {
+    const result = await axios.get(url);
+    console.log(result);
+  }
+
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <h1>RECI - V</h1>
+          <h1 onClick={getData}>RECI - V</h1>
           <h3>vegitarian and vegan recipes</h3>
         </div>
       </header>
 
       <div>
-        <form action=''>
-          <div>
-            <input type="radio" id="search" name="nav" value="search"/>
-            <img src={search} className="search-logo" alt="search logo" />
-            </div>
-          <div>
-            <input type="radio" id="saved" name="nav" value="saved" />
-            <img src={saved} className="saved-logo" alt="saved logo"/>
-            </div>
-          <div>
-            <input type="radio" id="list" name="nav" value="list" />
-            <img src={list} className="list-logo" alt="list logo"/>
-            </div>
-          <div></div>
-          <div>
-            <input type="radio" id="account" name="nav" value="account" />
-            <img src={account} className="account-logo" alt="account logo"/>
-            </div>
-          <div>
-            <input type="radio" id="settings" name="nav" value="settings" />
-            <img src={settings} className="settings-logo" alt="settings logo"/>
-            </div>
-          </form>
+        <Nav />
 
         <div className='content'>
-
+          {/* Recipe */}
+          {/* Recipe */}
         </div>
       </div>
-      {/* Recipe */}
-      {/* Recipe */}
+      
     </div>
   );
 }
